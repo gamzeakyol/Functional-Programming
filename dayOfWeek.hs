d0f35f12bb86b4b4cc3e98901d8ea9d6c467d400
gamzeakyol <gamze.akyol.3@gmail.com>
2018-02-25 01:42:26 +0300

    Update dayOfWeek.hs

--- dayOfWeek.hs
+++ dayOfWeek.hs
@@ -3,15 +3,8 @@ import Prelude
 
 dayOfWeek :: Integer -> Integer -> Integer -> Integer
 dayOfWeek y m d  
-	| m<=2 = n where n = m+12
-		
-	j = floor y/100
-	k = y 'mod' 100
-	
-	t1 = floor (13*(m+1)/5)
-	t2 = floor k/4
-	t3 = floor j/4
-	z = (d + t1 + k + t2 + t3 + 5 * j) 'mod' 7
-	
-	return z
+	| m <= 2 = (d + floor (fromIntegral(13*(n+1))/5) + (y mod 100) + floor ((y mod 100)/4) + ((floor (y/100)) / 4) + 5 * floor (y/100)) mod 7 
+	| otherwise = (d + floor (fromIntegral(13*(m+1))/5) + (y mod 100) + floor ((y mod 100)/4) + ((floor (y/100)) / 4) + 5 * floor (y/100)) mod 7
+	where 
+		n = m+12
 	
