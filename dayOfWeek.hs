4be82e07a30c0f2d25e8a0dbea677219bdf1d48c
gamzeakyol <gamze.akyol.3@gmail.com>
2018-02-25 02:12:52 +0300

    mod operator is changed.

--- dayOfWeek.hs
+++ dayOfWeek.hs
@@ -3,14 +3,14 @@ import Prelude
 
 dayOfWeek :: Integer -> Integer -> Integer -> Integer
 dayOfWeek y m d  
-	| m <= 2 = (d + t4 + k + t2 + t3 + (5 * j)) mod 7 
-	| otherwise = (d + t1 + k + t2 + t3 + (5 * j)) mod 7
+	| m <= 2 = (d + t4 + k + t2 + t3 + (5 * j)) `mod` 7 
+	| otherwise = (d + t1 + k + t2 + t3 + (5 * j)) `mod` 7
 	where 
-		n = m + 12
+		n = m+12
 		j = floor (y/100) 
-		k = y mod 100
-		t1 = floor (fromIntegral(13*(m+1))/5.0)
+		k = y `mod` 100
+		t1 = floor (fromIntegral (13*(m+1))/5.0)
 		t2 = floor (k/4)
-		t3 = (floor (y/100)) / 4
-		t4 = floor (fromIntegral(13*(n+1))/5.0)
+		t3 = (floor (y/100)) / 4.0
+		t4 = floor (fromIntegral (13*(n+1))/5.0)
 	
