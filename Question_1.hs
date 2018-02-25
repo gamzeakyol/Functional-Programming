be38de3d497c32be540e21940d56da3edeba9bc3
gamzeakyol <gamze.akyol.3@gmail.com>
2018-02-26 01:29:27 +0300

    y variable should be decremented in the first condition.

--- Question_1.hs
+++ Question_1.hs
@@ -3,14 +3,19 @@ import Prelude
 
 dayOfWeek :: Integer -> Integer -> Integer -> Integer
 dayOfWeek y m d  
-	| m <= 2 = (d + t4 + k + t2 + t3 + (5 * j)) `mod` 7 
+	| m <= 2    = (d + t1' + k' + t2' + t3' + (5 * j')) `mod` 7 
 	| otherwise = (d + t1 + k + t2 + t3 + (5 * j)) `mod` 7
-	where 
-		n = m+12
-		j = div y 100 
-		k = y `mod` 100
+	where
+		y' = y-1
+		m' = m+12
+		j  = div y 100 
+		k  = y `mod` 100
+		j' = div y' 100 
+		k' = y' `mod` 100
 		t1 = div (13*(m+1)) 5
+		t1' = div (13*(m'+1)) 5
 		t2 = div k 4
+		t2'= div k' 4
 		t3 = div j 4
-		t4 = div (13*(n+1)) 5
+		t3' = div j' 4
 	
