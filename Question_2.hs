6e32cea866f9509e8a91fa1993e4f909f6aabc11
gamzeakyol <gamze.akyol.3@gmail.com>
2018-02-26 04:19:48 +0300

    Update Question_2.hs

--- Question_2.hs
+++ Question_2.hs
@@ -1,3 +1,24 @@
+{-- dayOfWeek function is added to the top of this question as it is called from sundays1 function. --}
+
+dayOfWeek :: Integer -> Integer -> Integer -> Integer
+dayOfWeek y m d  
+	| m <= 2    = (d + t1' + k' + t2' + t3' + (5 * j')) `mod` 7 
+	| otherwise = (d + t1 + k + t2 + t3 + (5 * j)) `mod` 7
+	where
+		y' = y-1
+		m' = m+12
+		j  = div y 100 
+		k  = y `mod` 100
+		j' = div y' 100 
+		k' = y' `mod` 100
+		t1 = div (13*(m+1)) 5
+		t1' = div (13*(m'+1)) 5
+		t2 = div k 4
+		t2'= div k' 4
+		t3 = div j 4
+		t3' = div j' 4
+
+
 sundays1 :: Integer -> Integer -> Integer
 sundays1 start end = sundays' start 1 
 	where
@@ -9,3 +30,10 @@ sundays1 start end = sundays' start 1
 				nextY = if m == 12 then y+1 else y
 				nextM = if m < 12 then m+1 else m-11
 				rest = sundays' nextY nextM
+			
+-- Questions
+
+{-- The helper function sundays' calculates and sums up the total number of sundays in a given range of years. --}
+
+{-- If the rest variable was not used and its expression was used wherever it's needed, 
+    then readability of the code was decreased and modification of the code would be difficult. --}  
