6f499bafd9c7ed055e95bbeb0b587807aee5a744
gamzeakyol <gamze.akyol.3@gmail.com>
2018-02-26 03:45:19 +0300

    Update Question_2.hs

--- Question_2.hs
+++ Question_2.hs
@@ -3,9 +3,9 @@ sundays1 start end = sundays' start 1
 	where
 		sundays' :: Integer -> Integer -> Integer
 		sundays' y m
-			| y > end = rest 
+			| y > end = 0
 			| otherwise = if dayOfWeek y m 1 == 1 then rest + 1 else rest
 			where
-				nextY = y+1
-				nextM = m+1
-				rest = 0
+				nextY = if m == 12 then y+1 else y
+				nextM = if m < 12 then m+1 else m-11
+				rest = sundays' nextY nextM
