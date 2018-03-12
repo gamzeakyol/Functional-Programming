fe402e489c4a757fde12b1756ffa9c08e2129817
gamzeakyol <gamze.akyol.3@gmail.com>
2018-03-12 22:33:35 +0300

    Comments are added for functions.

--- Bonus1/Question_4.hs
+++ Bonus1/Question_4.hs
@@ -1,13 +1,15 @@
-leap :: Integer -> Bool 
+-- Leap year calculation, deciding that a year is a leap year or not
+leap :: Integer -> Bool
 leap y = (y `mod` 4 == 0) && (y `mod` 100 /= 0) || (y `mod` 400 == 0)
 
+-- Calculating number of days in a month
 daysInMonth :: Integer -> Integer -> Integer
 daysInMonth m y 
 	| m == 2 = if leap y then 29 else 28
 	| (m == 4) || (m == 6) || (m == 9) || (m == 11) = 30
 	| otherwise = 31
 	
-	
+-- Tail recursive version of sundays function	
 sundays2 :: Integer -> Integer -> Integer
 sundays2 start end = sundays' start 1 2
 	where
