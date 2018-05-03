e58d2e9806592e426a2cae47de77adb8dc7008a7
gamzeakyol <gamze.akyol.3@gmail.com>
2018-05-04 02:05:16 +0300

    Q1, Q2, Q3 and Q4 are created. Q2 will be revised later.
    wordCharCounts, sentenceCharCounts, dictCharCounts and dictWordsByCharCounts functions are created.

---
+++ Bonus3/Main.hs
@@ -0,0 +1,47 @@
+import Prelude hiding(filter)
+import Data.List 
+import Data.Map hiding(filter)
+import Data.Function (on)
+
+type Words = [Char]
+
+type Sentence = [Words]
+
+type CharacterCount = [(Char, Int)]
+
+-- Q1
+wordCharCounts :: Words -> CharacterCount
+wordCharCounts word = wordCharCounts' (sort word)
+	where
+		wordCharCounts' :: Words -> CharacterCount
+		wordCharCounts' [] = []
+		wordCharCounts' (w:word') = (w, (length (filter (==w) word))):(wordCharCounts' (removeChar w word'))
+			where
+				removeChar :: Char -> Words -> Words		
+				removeChar _ [] = []
+				removeChar x (y:ys) 
+					| x == y    = removeChar x ys
+					| otherwise = y : removeChar x ys
+
+
+-- Q2 
+--sentenceCharCounts :: Sentence -> CharacterCount
+--sentenceCharCounts [] = []
+--sentenceCharCounts (s:sentence) = assocs (fromListWith (++) concat (sentenceCharCounts' (s:sentence)))
+--    where
+--        sentenceCharCounts' :: Sentence -> [CharacterCount]
+--        sentenceCharCounts' (s:sentence) = (wordCharCounts s):(sentenceCharCounts' sentence)
+
+-- Q3
+dictCharCounts :: [Words] -> [(Words, CharacterCount)]
+dictCharCounts []         = []
+dictCharCounts (w:words') = (w, (wordCharCounts w)):(dictCharCounts words')
+
+-- Q4
+dictWordsByCharCounts :: [(Words, CharacterCount)] -> [[(Words, CharacterCount)]]
+dictWordsByCharCounts []                  = []
+dictWordsByCharCounts ws                  = groupBy ((==) `on` snd) ws
+
+
+
+
