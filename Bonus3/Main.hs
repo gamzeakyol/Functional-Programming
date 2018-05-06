67ebc18123fabfb8119ab3a39a8a38e1bdcae78e
gamzeakyol <gamze.akyol.3@gmail.com>
2018-05-07 01:43:15 +0300

    Q2 and Q4 are updated.
    Q2 works as described: sentenceCharCounts takes a sentence (e.g. ["i","love", "linux"]) and the helper function, sentenceCharCounts', also takes a sentence and returns the individual character counts of the each word. Then the  sentenceCharCounts function concatenates the sublists, which are character counts of individual words, then binds the tuples which have same key with fromListWith function.
    Q4 works as described: changeKeys helper function changes the keys of the tuples, and dictWordsByCharCounts function binds the words according to their keys (a.k.a their character counts).

--- Bonus3/Main.hs
+++ Bonus3/Main.hs
@@ -1,7 +1,6 @@
 import Prelude hiding(filter)
 import Data.List 
 import Data.Map hiding(filter)
-import Data.Function (on)
 
 type Words = [Char]
 
@@ -23,25 +22,26 @@ wordCharCounts word = wordCharCounts' (sort word)
 					| x == y    = removeChar x ys
 					| otherwise = y : removeChar x ys
 
-
 -- Q2 
---sentenceCharCounts :: Sentence -> CharacterCount
---sentenceCharCounts [] = []
---sentenceCharCounts (s:sentence) = assocs (fromListWith (++) concat (sentenceCharCounts' (s:sentence)))
---    where
---        sentenceCharCounts' :: Sentence -> [CharacterCount]
---        sentenceCharCounts' (s:sentence) = (wordCharCounts s):(sentenceCharCounts' sentence)
+sentenceCharCounts :: Sentence -> CharacterCount
+sentenceCharCounts [] = []
+sentenceCharCounts (s:sentence) = assocs (fromListWith (+) (concat (sentenceCharCounts' (s:sentence))))
+	where
+		sentenceCharCounts' :: Sentence -> [CharacterCount]
+		sentenceCharCounts' sentence@(s:sentence')
+			| sentence == []  = []
+			| sentence == [s] = [wordCharCounts s]
+			| otherwise       = (wordCharCounts s):(sentenceCharCounts' sentence')
 
 -- Q3
 dictCharCounts :: [Words] -> [(Words, CharacterCount)]
 dictCharCounts []         = []
 dictCharCounts (w:words') = (w, (wordCharCounts w)):(dictCharCounts words')
 
--- Q4
-dictWordsByCharCounts :: [(Words, CharacterCount)] -> [[(Words, CharacterCount)]]
-dictWordsByCharCounts []                  = []
-dictWordsByCharCounts ws                  = groupBy ((==) `on` snd) (sort ws)
-
-
-
-
+--Q4
+dictWordsByCharCounts :: [(Words, CharacterCount)] -> [(CharacterCount, [Words])]
+dictWordsByCharCounts ws = assocs (fromListWith (++) (changeKeys ws))
+	where
+		changeKeys :: [(Words, CharacterCount)] -> [(CharacterCount, [Words])]
+		changeKeys [] = []
+		changeKeys ((w, c):ws) = (c, [w]):(changeKeys ws)
