a5ecd20da8fa28d488b0988b8cb6818adf28b7ac
gamzeakyol <gamze.akyol.3@gmail.com>
2018-05-07 03:54:16 +0300

    Comments are added to  functions.
    Comments are added to show example inputs and outputs of the functions.

--- Bonus3/Main.hs
+++ Bonus3/Main.hs
@@ -9,6 +9,8 @@ type Sentence = [Words]
 type CharacterCount = [(Char, Int)]
 
 -- Q1
+-- ex. input: wordCharCounts "tea"
+-- ex. output: [('a',1),('e',1),('t',1)]
 wordCharCounts :: Words -> CharacterCount
 wordCharCounts word = wordCharCounts' (sort word)
 	where
@@ -22,7 +24,9 @@ wordCharCounts word = wordCharCounts' (sort word)
 					| x == y    = removeChar x ys
 					| otherwise = y : removeChar x ys
 
--- Q2 
+-- Q2
+-- ex. input: sentenceCharCounts ["i", "love", "linux"]
+-- ex. output: [('e',1),('i',2),('l',2),('n',1),('o',1),('u',1),('v',1),('x',1)]
 sentenceCharCounts :: Sentence -> CharacterCount
 sentenceCharCounts [] = []
 sentenceCharCounts (s:sentence) = assocs (fromListWith (+) (concat (sentenceCharCounts' (s:sentence))))
@@ -34,11 +38,15 @@ sentenceCharCounts (s:sentence) = assocs (fromListWith (+) (concat (sentenceChar
 			| otherwise       = (wordCharCounts s):(sentenceCharCounts' sentence')
 
 -- Q3
+-- ex. input: dictCharCounts ["tea", "all", "ate"]
+-- ex. output: [("tea",[('a',1),('e',1),('t',1)]),("all",[('a',1),('l',2)]),("ate",[('a',1),('e',1),('t',1)])]
 dictCharCounts :: [Words] -> [(Words, CharacterCount)]
 dictCharCounts []         = []
 dictCharCounts (w:words') = (w, (wordCharCounts w)):(dictCharCounts words')
 
 --Q4
+--ex. input: dictWordsByCharCounts (dictCharCounts ["tea", "all", "ate"])
+--ex. output: [([('a',1),('e',1),('t',1)],["ate","tea"]),([('a',1),('l',2)],["all"])]
 dictWordsByCharCounts :: [(Words, CharacterCount)] -> [(CharacterCount, [Words])]
 dictWordsByCharCounts ws = assocs (fromListWith (++) (changeKeys ws))
 	where
@@ -47,6 +55,8 @@ dictWordsByCharCounts ws = assocs (fromListWith (++) (changeKeys ws))
 		changeKeys ((w, c):ws) = (c, [w]):(changeKeys ws)
 		
 --Q5
+--ex. input:  wordAnagrams "eat" (dictWordsByCharCounts (dictCharCounts ["tea", "all", "ate"]))
+--ex. output: ["ate","tea"]
 wordAnagrams :: Words -> [(CharacterCount, [Words])] -> [Words]
 wordAnagrams word ws@((c, w):ws')
 	| ws == []                  = []
@@ -57,6 +67,8 @@ wordAnagrams word ws@((c, w):ws')
 --charCountsSubsets :: CharacterCount -> [[CharacterCount]]
 	
 --Q7
+-- ex. input: subtractCounts (wordCharCounts "seat") (wordCharCounts "ate")
+-- ex. output: [('a',0),('e',0),('s',1),('t',0)]
 subtractCounts :: CharacterCount -> CharacterCount -> CharacterCount
 subtractCounts s1'@((w1, c1):s1) s2'@((w2, c2):s2)
 	| s2' == []                = s1'
